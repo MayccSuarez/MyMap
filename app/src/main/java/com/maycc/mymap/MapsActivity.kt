d02a2707a6d08229ed27a6c8914d1fe688d7df1c
= <=>
2018-09-03 19:18:12 -0500

    implementar ubicación actual

--- app/src/main/java/com/maycc/mymap/MapsActivity.kt
+++ app/src/main/java/com/maycc/mymap/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.maycc.mymap
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -14,13 +20,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private val locationRequest = LocationRequest()
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+        initLocationRequest()
+        initLocationCallback()
+        settingConstantLocation()
     }
 
     /**
@@ -39,5 +55,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun initLocationRequest() {
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    private fun initLocationCallback() {
+        locationCallback = object: LocationCallback() {
+
+            override fun onLocationResult(locationResult: LocationResult?) {
+                super.onLocationResult(locationResult)
+
+                for (location in locationResult?.locations!!) {
+                    val longitude = location.longitude
+                    val latitude = location.latitude
+
+                    Toast.makeText(applicationContext, "$longitude $latitude", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun settingConstantLocation() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
 }
